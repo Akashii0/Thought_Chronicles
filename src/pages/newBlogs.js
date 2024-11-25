1ca780168477c7b966bbfe81ac6989c3a69b5f98
ZHADOW999 <babalolav9@gmail.com>
2024-11-25 14:35:42 +0100

    new

--- src/pages/newBlogs.js
+++ src/pages/newBlogs.js
@@ -21,6 +21,7 @@ const NewBlog = () => {
         .catch((error) => {
             console.error("There was an error creating the blog post!", error);
             setIspending(false);
+            
         });
     }
 
