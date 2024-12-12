e4ea02bd68c5820016713b40f089a71a72592801
Akashii0 <abdulkid151@gmail.com>
2024-12-12 13:48:43 +0100

    Minor fixes to users route

--- app/routers/user.py
+++ app/routers/user.py
@@ -2,8 +2,16 @@
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from typing_extensions import Annotated
 from sqlalchemy.exc import IntegrityError
-from fastapi import APIRouter, Depends, File, Form, HTTPException, Request, UploadFile, status
-from fastapi.responses import FileResponse, JSONResponse, RedirectResponse
+from fastapi import (
+    APIRouter,
+    Depends,
+    File,
+    HTTPException,
+    Request,
+    UploadFile,
+    status,
+)
+from fastapi.responses import FileResponse, JSONResponse
 from sqlalchemy.orm import Session
 import app.oauth2 as oauth2
 import app.models as models
@@ -14,27 +22,25 @@ import shutil
 import uuid
 import os
 
-router = APIRouter(
-    tags=["Users"],
-    prefix="/api/users"
-    )
+router = APIRouter(tags=["Users"], prefix="/api/users")
 
 UPLOAD_DIR = Path("profile_pictures")
 UPLOAD_DIR.mkdir(parents=True, exist_ok=True)
 
+
 @router.post("/")
 def create_user(
-    form_data:  Annotated[OAuth2PasswordRequestForm, Depends()],
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
     request: Request,
     db: Session = Depends(get_db),
 ):
-
     hashed_password = utils.hash(form_data.password)
-    existing_user = db.query(models.User).filter(models.User.author == form_data.username).first()
+    existing_user = (
+        db.query(models.User).filter(models.User.author == form_data.username).first()
+    )
     if existing_user:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Username already exists"
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Username already exists"
         )
 
     # new_user = User(**user.model_dump(username=username, password=password))
@@ -43,18 +49,17 @@ def create_user(
         db.add(new_user)
         db.commit()
         # db.refresh(new_user)
-    
+
     except IntegrityError:
         db.rollback()
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Author already exists"
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Author already exists"
         )
 
     access_token = oauth2.create_access_token(data={"user_id": form_data.username})
     # print(new_user)
     # return RedirectResponse(url="/login")
-    
+
     # return JSONResponse(
     #     content={
     #         "access_token": access_token,
@@ -62,24 +67,22 @@ def create_user(
     #         "author": author
     #     }
     # )
-    
+
     response = JSONResponse(
-        content={
-            "message": "User Created Successfully",
-            "author": form_data.username
-        }
+        content={"message": "User Created Successfully", "author": form_data.username}
     )
 
     response.set_cookie(
         key="token",
         value=access_token,
         httponly=True,  # Prevents JavaScript access
-        secure=True,    # Only sends cookie over HTTPS
-        samesite="none", # Provides CSRF protection
-        max_age=1800    # 30 minutes in seconds
+        secure=True,  # Only sends cookie over HTTPS
+        samesite="none",  # Provides CSRF protection
+        max_age=1800,  # 30 minutes in seconds
     )
 
     return response
+
 
 @router.get("/{user_id}/profile-picture")
 def get_pfps(
@@ -89,11 +92,11 @@ def get_pfps(
     user = db.query(models.User).filter(models.User.id == user_id).first()
     if not user or not user.profile_picture:
         raise HTTPException(status_code=404, detail="Profile picture not found.")
-    
+
     file_path = Path(user.profile_picture)
     if not file_path.exists():
         raise HTTPException(status_code=404, detail="Profile picture file not found.")
-    
+
     return FileResponse(file_path, media_type="image/jpeg")
     # return {"profile_picture": f"/{user.profile_picture}"}
 
@@ -103,21 +106,22 @@ async def upload_profile_picture(
     user_id: int,
     file: UploadFile = File(...),
     db: Session = Depends(get_db),
-    current_user: int = Depends(oauth2.get_current_user)
+    current_user: int = Depends(oauth2.get_current_user),
 ):
-    
     user = db.query(models.User).filter(models.User.id == user_id).first()
     if not user:
         raise HTTPException(status_code=404, detail="User not found.")
-    
+
     if user_id != current_user.id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                            detail="Not authorized to perform requested action")
-    
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Not authorized to perform requested action",
+        )
+
     # Validate file type, IF its an img or video/music. hehe
     if not file.content_type.startswith("image/"):
         raise HTTPException(status_code=400, detail="Only image files are allowed.")
-    
+
     # # Delete old profile picture if it exists
     # if user.profile_picture:
     #     old_file_path = Path(user.profile_picture)
@@ -127,7 +131,7 @@ async def upload_profile_picture(
     try:
         # Retrieve the old profile picture path
         old_file_path = user.profile_picture
-        
+
         # If the old profile picture exists, remove it
         if old_file_path and os.path.exists(old_file_path):
             os.remove(old_file_path)
@@ -148,8 +152,10 @@ async def upload_profile_picture(
         db.commit()
         db.refresh(user)
 
-        return {"message": "Profile picture updated successfully.", "profile_picture": str(file_path)}
+        return {
+            "message": "Profile picture updated successfully.",
+            "profile_picture": str(file_path),
+        }
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
-
